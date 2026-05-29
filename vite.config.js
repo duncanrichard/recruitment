33b32dacdec44f1d8e1b9c40e0b0da8cee3a669d
Duncan <richardpratama9898@gmail.com>
2026-05-29 15:38:05 +0700

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/css/app.css',
-                'resources/js/app.js',
+                'resources/js/app.jsx',
                 'resources/js/pages/admin/index.jsx',
                 'resources/js/pages/pendaftaran/Index.jsx',
             ],
